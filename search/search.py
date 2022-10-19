cffc448628d52ee8a62ceea7f03eec64cd685230
Adalberto Teixeira Guedes <48949370+rssadal@users.noreply.github.com>
2022-10-19 17:03:12 -0300

    Update search.py

--- search/search.py
+++ search/search.py
@@ -70,111 +70,106 @@ def tinyMazeSearch(problem):
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
 
-def depthFirstSearch(problem):  """"DFS"""
+def depthFirstSearch(problem):  """DFS"""
     
     no = {'estado':problem.getStartState(),'cost':0}  """Recebe o lugar onde nasce o pacman"""
     if problem.isGoalState(no['estado']): """Verifica se já está no destino"""
         return []
     abertos = []
-    abertos.append(no) """"Adiciona o no inicial em abertos"""
+    abertos.append(no) """Adiciona o no inicial em abertos"""
     fechados = set()
     while True:
         if not abertos: 
             raise Exception('falha na busca')
-        no = abertos.pop() """"Retiramos o no de abertos"""
+        no = abertos.pop() """Retiramos o no de abertos"""
         fechados.add(no['estado']) 
-        filhos = problem.getSuccessors(no['estado']) """"Buscamos todos os filhos desse nó"""
-        for filho in filhos: """"Verificamos filho a filho encontrado"""
-            child = {'estado':filho[0],'acao':filho[1],'custo':filho[2],'pai':no} """"Definimos um dicionario filho para que possamos comparar e encontrar o caminho"""
-            if child['estado']not in fechados:  """"Verificação para caso o filho não esteja em fechados"""
-                if problem.isGoalState(child['estado']): """"Verificação para caso o filho que está sendo avaliado seja o objetivo"""
+        filhos = problem.getSuccessors(no['estado']) """Buscamos todos os filhos desse nó"""
+        for filho in filhos: """Verificamos filho a filho encontrado"""
+            child = {'estado':filho[0],'acao':filho[1],'custo':filho[2],'pai':no} """Definimos um dicionario filho para que possamos comparar e encontrar o caminho"""
+            if child['estado']not in fechados:  """Verificação para caso o filho não esteja em fechados"""
+                if problem.isGoalState(child['estado']): """Verificação para caso o filho que está sendo avaliado seja o objetivo"""
                     acao = []
                     no = child
-                    while 'pai' in no: """"Encontrando caminho feito a partir dos pais"""
+                    while 'pai' in no: """Encontrando caminho feito a partir dos pais"""
                         acao.append(no['acao'])
                         no = no['pai']
-                    acao.reverse() """"Inverte o array"""
-                    return acao """"Retorna o caminho"""
+                    acao.reverse() """Inverte o array"""
+                    return acao """Retorna o caminho"""
                 abertos.insert(0,child)
 
-def breadthFirstSearch(problem): """"BFS"""
+def breadthFirstSearch(problem): """BFS"""
     no = {'estado':problem.getStartState(),'cost':0} """Recebe o lugar onde nasce o pacman"""
     if problem.isGoalState(no['estado']): """Verifica se já está no destino"""
         return []
     abertos = util.Queue() 
-    abertos.push(no) """"Adiciona o no inicial em abertos"""
+    abertos.push(no) """Adiciona o no inicial em abertos"""
     fechados = set()
     while True:
-        if abertos.isEmpty():  """"caso não aja mais caminhos a explorar retorna erro"""
+        if abertos.isEmpty():  """caso não aja mais caminhos a explorar retorna erro"""
             raise Exception('falha na busca')
-        no = abertos.pop()  """"Retiramos o no de abertos"""
+        no = abertos.pop()  """Retiramos o no de abertos"""
         fechados.add(no['estado'])   
-        filhos = problem.getSuccessors(no['estado'])  """"Buscamos todos os filhos desse nó"""
-        for filho in filhos:  """"Verificamos filho a filho encontrado"""
-            child = {'estado':filho[0],'acao':filho[1],'custo':filho[2],'pai':no} """"Definimos um dicionario filho para que possamos comparar e encontrar o caminho"""
-            if child['estado']not in fechados:  """"Verificação para caso o filho não esteja em fechados"""
-                if problem.isGoalState(child['estado']):  """"Verificação para caso o filho que está sendo avaliado seja o objetivo"""
+        filhos = problem.getSuccessors(no['estado'])  """Buscamos todos os filhos desse nó"""
+        for filho in filhos:  """Verificamos filho a filho encontrado"""
+            child = {'estado':filho[0],'acao':filho[1],'custo':filho[2],'pai':no} """Definimos um dicionario filho para que possamos comparar e encontrar o caminho"""
+            if child['estado']not in fechados:  """Verificação para caso o filho não esteja em fechados"""
+                if problem.isGoalState(child['estado']):  """Verificação para caso o filho que está sendo avaliado seja o objetivo"""
                     acao = []
                     no = child
-                    while 'pai' in no: """"Encontrando caminho feito a partir dos pais"""
+                    while 'pai' in no: """Encontrando caminho feito a partir dos pais"""
                         acao.append(no['acao'])
                         no = no['pai']
-                    acao.reverse() """"Inverte o array"""
-                    return acao """"Retorna o caminho"""
+                    acao.reverse() """Inverte o array"""
+                    return acao """Retorna o caminho"""
                 abertos.push(child)
 
-def uniformCostSearch(problem):
-    abertos = util.PriorityQueue() #cria uma lista de estados que nao foram visitados
-    fechados = [] #cria uma lista de estados que ja foram visitados
-    abertos.push((problem.getStartState(), [], 0), 0) #add na fila um objeto que possui estado um antecessor um custo do estado
-    while not abertos.isEmpty(): #enquanto a lista de itens nao visitados ainda conter estados
+def uniformCostSearch(problem): """Busca de custo uniforme"""
+    abertos = util.PriorityQueue() """cria uma lista de estados que nao foram visitados"""
+    fechados = []  """cria uma lista de estados que ja foram visitados"""
+    abertos.push((problem.getStartState(), [], 0), 0) """add na fila um objeto que possui estado um antecessor um custo do estado"""
+    while not abertos.isEmpty(): """enquanto a lista de itens nao visitados ainda conter estados"""
         pass
     return []
-    #guarda o objeto nas variaveis
-    aux = abertos.pop()
+    """guarda o objeto nas variaveis"""
+    aux = abertos.pop() 
     filho = aux
     caminhos = aux
     valor = aux
-    if(not filho in fechados): #se o filho gerado visitado nao estiver na fila de visitados
-        fechados.append(filho) #adiciona na lista de visitados
-        if problem.isGoalState(filho): #se o problema atual for o objetivo
-            return caminhos #retorna o caminho do estado de objeto
+    if(not filho in fechados): """se o filho gerado visitado nao estiver na fila de visitados"""
+        fechados.append(filho) """adiciona na lista de visitados"""
+        if problem.isGoalState(filho): """se o problema atual for o objetivo"""
+            return caminhos """retorna o caminho do estado de objeto"""
         for child, direcao, heurisValue in problem.getSuccessors(filho):
-            #para cada crianca gerada, direcao e valor da heuristica no estado sucessor
+            """para cada crianca gerada, direcao e valor da heuristica no estado sucessor"""
             abertos.push((child, caminhos+[direcao], valor + heurisValue), valor + heurisValue)
-            #adiciona na lista de nao visitados essas variaveis, o sucessor, o caminho + direcao, que e o pai de onde veio, e os valores heuristicos
+            """adiciona na lista de nao visitados essas variaveis, o sucessor, o caminho + direcao, que e o pai de onde veio, e os valores heuristicos"""
 
 
 def nullHeuristic(state, problem=None):
-    """
-    A heuristic function estimates the cost from the current state to the nearest
-    goal in the provided SearchProblem.  This heuristic is trivial.
-    """
     return 0
 
 def aStarSearch(problem, heuristic=nullHeuristic):
-    abertos = util.PriorityQueue() #cria uma lista de estados que nao foram visitados
-    abertos.push((problem.getStartState(), [], 0), heuristic(problem.getStartState(), problem) ) #add na fila um objeto que possui estado um antecessor um custo do estado
+    abertos = util.PriorityQueue() """cria uma lista de estados que nao foram visitados """
+    abertos.push((problem.getStartState(), [], 0), heuristic(problem.getStartState(), problem) ) """add na fila um objeto que possui estado um antecessor um custo do estado"""
     fechados = []
-    while not abertos.isEmpty(): ##enquanto a lista de itens nao visitados ainda conter estados
-        #guarda o objeto nas variaveis
+    while not abertos.isEmpty(): """enquanto a lista de itens nao visitados ainda conter estados"""
+        """guarda o objeto nas variaveis"""
         aux = abertos.pop()
         filhos = aux
         caminhos = aux
         valor = aux
-        if(not filhos in fechados): #se o filho gerado visitado nao estiver na fila de visitados
-            fechados.append(filhos) #adiciona na lista de visitados
-            if problem.isGoalState(filhos): #se o problema atual for o objetivo
+        if(not filhos in fechados): """se o filho gerado visitado nao estiver na fila de visitados"""
+            fechados.append(filhos) """adiciona na lista de visitados"""
+            if problem.isGoalState(filhos):"""se o problema atual for o objetivo"""
                 return caminhos #retorna o caminho do estado de objeto
             for child, direcao, heurisValue in problem.getSuccessors(filhos):
-                #para cada crianca gerada, direcao e valor da heuristica no estado sucessor
+                """para cada crianca gerada, direcao e valor da heuristica no estado sucessor"""
                 g = valor + heurisValue
-                #adiciona um valor para a heuristica
+               """adiciona um valor para a heuristica"""
                 abertos.push((child, caminhos+[direcao], valor + heurisValue), g + heuristic(child, problem))
-                #adiciona na lista de nao visitados essas variaveis, o sucessor, o caminho + direcao, que e o pai de onde veio, e os valores heuristicos
+               """adiciona na lista de nao visitados essas variaveis, o sucessor, o caminho + direcao, que e o pai de onde veio, e os valores heuristicos"""
     return []
 
-# Abbreviations
 bfs = breadthFirstSearch
 dfs = depthFirstSearch
 astar = aStarSearch
